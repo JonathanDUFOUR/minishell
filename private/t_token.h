c7e67fb530984f692f67c07e5f64ef551075b96a
jodufour <jodufour@student.42.fr>
2021-11-24 16:31:06 +0100

    Structure files   [DOING] Read Posix manual [DOING] Begin to code     [TODO]

--- private/t_token.h
+++ private/t_token.h
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   e_ret.h                                            :+:      :+:    :+:   */
+/*   t_token.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 11:12:02 by jodufour          #+#    #+#             */
-/*   Updated: 2021/11/24 11:12:52 by jodufour         ###   ########.fr       */
+/*   Created: 2021/11/24 14:24:26 by jodufour          #+#    #+#             */
+/*   Updated: 2021/11/24 16:26:27 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef E_RET_H
-# define E_RET_H
+#ifndef T_TOKEN_H
+# define T_TOKEN_H
 
-enum	e_ret
+typedef struct s_token	t_token;
+
+struct	s_token
 {
-	SUCCESS
+	char const	*str;
+	t_token		*next;
+	t_token		*prev;
 };
 
 #endif
--- private/t_token_lst.h
+++ private/t_token_lst.h
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   t_token_lst.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 11:11:21 by jodufour          #+#    #+#             */
-/*   Updated: 2021/11/24 16:28:56 by jodufour         ###   ########.fr       */
+/*   Created: 2021/11/24 16:26:41 by jodufour          #+#    #+#             */
+/*   Updated: 2021/11/24 16:28:44 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "e_ret.h"
+#ifndef T_TOKEN_LST_H
+# define T_TOKEN_LST_H
 
-int	main(void)
+# include <stddef.h>
+# include "t_token.h"
+
+typedef struct s_token_lst	t_token_lst;
+
+struct s_token_lst
 {
-	return (SUCCESS);
-}
+	t_token	*head;
+	t_token	*tail;
+	size_t	size;
+};
+
+#endif
