36df90c326499cbfb4bec1387b6b8a3cd226206a
jodufour <jodufour@student.42.fr>
2021-11-30 11:56:06 +0100

    Remove tests from main.c [OK]

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:11:21 by jodufour          #+#    #+#             */
-/*   Updated: 2021/11/30 11:39:30 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/11/30 11:55:44 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,5 @@
 
 int	main(void)
 {
-	char	*command_line;
-
-	command_line = readline("$> ");
-	printf("line: %s\n", command_line);
-	free(command_line);
 	return (SUCCESS);
 }
