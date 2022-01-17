418cebed7a784ff1748eb448b5b58c1ad15a88a8
majacque <majacque@student.42.fr>
2022-01-17 19:22:15 +0100

    msh_echo() --> handle multiple n option    [OK]

--- srcs/builtins/echo/core.c
+++ srcs/builtins/echo/core.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   core.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: majacque <majacque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:18:47 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/15 10:23:37 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/01/17 19:16:37 by majacque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 #include "ft_string.h"
 #include "t_token_lst.h"
 
+static bool	__is_n(const char *const str)
+{
+	int	i;
+
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	msh_echo(t_env_lst *const env __attribute__((unused)), t_token *const token)
 {
 	t_token const	*args = token->next;
@@ -24,7 +38,7 @@ int	msh_echo(t_env_lst *const env __attribute__((unused)), t_token *const token)
 	option_n = false;
 	if (args != NULL)
 	{
-		if (args->type == T_OPTION && !ft_strcmp("-n", args->str))
+		while (args && args->type == T_OPTION && __is_n(args->str))
 		{
 			args = args->next;
 			option_n = true;
