c3a1565618c336b3785fd2d18c5acbe99ea0ebdb
Jonathan DUFOUR <jodufour@e2r10p16.clusters.42paris.fr>
2022-01-20 09:37:23 +0100

    Fix unexpected newline [OK]

--- srcs/token/lst/token_lst_syntax_check.c
+++ srcs/token/lst/token_lst_syntax_check.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 21:45:15 by jodufour          #+#    #+#             */
-/*   Updated: 2022/01/15 01:54:54 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/01/20 09:34:14 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ static int	__error_msg(t_token const *const node, char const *program)
 	g_exit_status = 2;
 	ft_putstr_fd(program, STDERR_FILENO);
 	ft_putstr_fd(": syntax error near unexpected token `", STDERR_FILENO);
-	ft_putstr_fd(node->str, STDERR_FILENO);
+	if (node)
+		ft_putstr_fd(node->str, STDERR_FILENO);
+	else
+		ft_putstr_fd("newline", STDERR_FILENO);
 	ft_putstr_fd("'\n", STDERR_FILENO);
 	return (EXIT_FAILURE);
 }
@@ -35,20 +38,23 @@ int	token_lst_syntax_check(t_token_lst *const tokens, char const *program)
 
 	while (curr)
 	{
-		if ((curr->prev && (curr->prev->type == T_REDIRECTIN
-					|| curr->prev->type == T_REDIRECTOUT)
-				&& curr->type != T_FILE
-				&& curr->type != T_DELIMITER)
-			|| (curr->prev && curr->prev->type == T_PIPE
-				&& curr->type != T_BUILTIN
-				&& curr->type != T_COMMAND)
-			|| (curr == tokens->head
-				&& curr->type == T_PIPE)
-			|| (curr == tokens->tail
+		if (curr == tokens->head && curr->type == T_PIPE)
+			return (__error_msg(curr, program));
+		else if ((curr == tokens->tail
 				&& (curr->type == T_PIPE
 					|| curr->type == T_REDIRECTIN
-					|| curr->type == T_REDIRECTOUT)))
-			return (__error_msg(curr, program));
+					|| curr->type == T_REDIRECTOUT))
+			|| (curr->type == T_PIPE
+				&& curr->next->type != T_BUILTIN
+				&& curr->next->type != T_COMMAND
+				&& curr->next->type != T_REDIRECTIN
+				&& curr->next->type != T_REDIRECTOUT)
+			|| (curr->type == T_REDIRECTIN
+				&& curr->next->type != T_FILE
+				&& curr->next->type != T_DELIMITER)
+			|| (curr->type == T_REDIRECTOUT
+				&& curr->next->type != T_FILE))
+			return (__error_msg(curr->next, program));
 		curr = curr->next;
 	}
 	return (EXIT_SUCCESS);
